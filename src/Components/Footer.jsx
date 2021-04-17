d7e7deefd5fa34bd6b833a90bc98e350c6e207a8
Maria Camila <mariacmaza7@gmail.com>
2021-04-16 22:31:48 -0500

    Se crearon carpetas para los componentes

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer(){
+    return (
+        <div className="footer">
+            <h5>Informacion:</h5>
+            <p>Maria Maza </p>
+        </div>
+    )
+}
+
+export default Footer;
